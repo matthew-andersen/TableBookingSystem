283c58e3c1a01508c0ff92d1fc828a9fbbca7bb6
Draga <draga.doncilapop@my.jcu.edu.au>
2016-12-08 15:07:58 +1000

    tidied up and added more comments to script file

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@ var AVAILABLE = "rgb(94, 172, 95)";
 var UNAVAILABLE = "rgb(165, 165, 140)";
 var SELECTED = "rgb(41, 145, 42)";
 
+var roomList = ['room_1', 'room_2', 'room_3', 'room_4', 'desk_1', 'desk_2', 'desk_3', 'desk_4', 'desk_5', 'desk_6', 'desk_7', 'desk_8'];
 // List that stores all the selected rooms of the user
 var userOrder = [];
-var roomList = ['room_1', 'room_2', 'room_3', 'room_4', 'desk_1', 'desk_2', 'desk_3', 'desk_4', 'desk_5', 'desk_6', 'desk_7', 'desk_8'];
 
 // Global variables to handle date/time - both current and onscreen date/time
 var todayDate = new Date();
@@ -22,6 +22,7 @@ var currentHour = todayDate.getHours();
 var currentDateTime = moment([currentYear, currentMonth, currentDay, currentHour]);
 var onScreenDate = moment([currentYear, currentMonth, currentDay, currentHour]);
 
+//Updates banked days/hours for user any time they make a booking/refresh the page
 function updateUserInfoView(userInfo) {
     var spanTags = document.getElementsByTagName("SPAN");
     for (var i = 1; i < userInfo.length; i++) {
@@ -30,7 +31,7 @@ function updateUserInfoView(userInfo) {
 }
 
 // Initialises the application on page load/refresh
-// Sets the date, and displays room availability via colouring
+// Sets the date, updates the view based on availabilities, updates the user's account information
 $(document).ready(function roomGen() {
     initialiseDate();
     var currentBookings = getBookings();
@@ -45,8 +46,7 @@ function initialiseDate() {
     document.getElementById("date-display-box").innerHTML = moment(onScreenDate).format('MMMM Do YYYY - h:00a');
 }
 
-// Function checks if location has been booked and if it is not it allows the user to make a booking also includes basic error checking
-// The booking is then confirmed by the user and the location is 'greyed-out'
+//Returns true if the first parameter is a positive number smaller than or equal to the second parameter
 function isValidNumDays(days, numDaysRemaining) {
     //returns true if days is a number, greater than 0 and within the num of days left to the user.
     // return (!isNaN(days) && (days > 0) && (days <= numDaysRemaining));
@@ -72,21 +72,32 @@ function handleLocationSelection(name, location) {
     if (room_svg_object.style.fill == UNAVAILABLE) {
         alert("Sorry, this has been booked");
     } else if (room_svg_object.style.fill == SELECTED) {
-        alert("This booking needs to be confirmed")
+        alert("This booking needs to be confirmed");
     } else {
         // Get the user account information from the database;
         var accountInfo = getUserAccount();
         var numDaysRemaining = accountInfo[1];
+
+        //Ask user how long they would like to book for
         var days = window.prompt("Enter how many days").trim();
         if (isValidNumDays(days, numDaysRemaining)) {
             var cost = days * 25;
+
+            //if user confirms addition to cart
             if (window.confirm("Your booking is for " + days + " days. This will cost a total of: $" + cost + ". Press OK to confirm.") == true) {
+                //calculate the end datetime of the booking
                 var bookingDateTime = onScreenDate.clone();
                 bookingDateTime.add(days, 'days');
+
+                //if it does not conflict with existing bookings
                 if (isValidEndDatetime(bookingDateTime, name)) {
                     room_svg_object.style.fill = SELECTED;
-                    userOrder.push(['4', currentDateTime.format('YYYYMMDD'), '14', days.toString(), '24', '0', onScreenDate.format('YYYY-MM-DD HH:mm:ss'), bookingDateTime.format('YYYY-MM-DD HH:mm:ss'), name]);
+
                     //pushing: user_id, date_created, num_days, num_desk_hours, num_room_hours, start_datetime, end_datetime, location_id
+                    //populate the list with relevant order information for sending to database
+                    userOrder.push(['4', currentDateTime.format('YYYYMMDD'), '14', days.toString(), '24', '0', onScreenDate.format('YYYY-MM-DD HH:mm:ss'), bookingDateTime.format('YYYY-MM-DD HH:mm:ss'), name]);
+
+                    //add to cart
                     document.getElementById("cart-box-order").innerHTML = document.getElementById("cart-box-order").innerHTML + "<p>" + location + " for " + days + " days: $" + cost + "</p>";
                 }
                 else {
@@ -94,9 +105,6 @@ function handleLocationSelection(name, location) {
                 }
             }
         }
-        // else {
-        //     window.alert("Please enter a valid booking duration")
-        // }
     }
 }
 
@@ -105,6 +113,7 @@ function isValidEndDatetime(bookingEndDatetime, location) {
     bookingEndDatetime = moment(bookingEndDatetime, "YYYY-MM-DD HH:mm:ss");
     var currentBookings = getBookings();
     for (var i = 0; i < currentBookings.length; i++) {
+
         var startDatetimeString = currentBookings[i][1];
         var endDatetimeString = currentBookings[i][2];
         var bookingLocation = currentBookings[i][0];
@@ -112,7 +121,7 @@ function isValidEndDatetime(bookingEndDatetime, location) {
         var startDateTime = moment(startDatetimeString, "YYYY-MM-DD HH:mm:ss");
         var endDateTime = moment(endDatetimeString, "YYYY-MM-DD HH:mm:ss");
 
-        //if this booking pertains to the location trying to be booked
+        //if this iteration's booking pertains to the location trying to be booked
         if (bookingLocation == location) {
             // If the end datetime of this potential booking conflicts with an existing booking
             if (bookingEndDatetime.isBetween(startDateTime, endDateTime) || bookingEndDatetime.isSame(endDateTime)) {
@@ -120,6 +129,7 @@ function isValidEndDatetime(bookingEndDatetime, location) {
             }
         }
     }
+    //booking must be valid
     return true;
 }
 
@@ -131,28 +141,27 @@ function getUserAccount() {
         // What to you want to do with the response?
         userAccountInfo = this.responseText.split(',');
     };
+
     // Open the file to make the request. Need to be false so program execution halts until response is ready
     userRequest.open("GET", "php/get_user_account.php", false);
-
     userRequest.send();
+
     return userAccountInfo;
 }
 
 // Sends all the rooms the user ordered to be processed then resets the 'cart' and reloads the page
 function submitOrder() {
+    //sends order information to booking records
     $.ajax({
         url: 'php/order_process.php',
         type: 'POST',
         data: {'q': JSON.stringify(userOrder)}
-        // success: function(data) {
-        //     window.alert(data);
-        // }
     });
-    // newRequest.open("POST", "order_process.php?q=" + newArr, true);
-    // newRequest.send();
+
     alert("Thank you for your booking!");
     $("#cart-box-order").text("");
     location.reload();
+
     //user order indices: 3, 4, 5
     var numDays = userOrder[0][3];
     var numDeskHours = userOrder[0][4];
@@ -169,17 +178,15 @@ function submitOrder() {
     // else if (numRoomHours != 0){
     //     currentAccountInfo[3] -= numRoomHours;
     // }
+
+    //sends updated account information to user table
     $.ajax({
         url: 'php/update_user_account.php',
         type: 'POST',
         data: {'q': JSON.stringify(currentAccountInfo)}
-        // success: function () {
-        //     window.alert(data);
-        // }
     });
 }
 
-// Essentially the Magnum Opus of this JS file... lmao
 // Used to go back/forward in day/time - won't allow the user to go behind the current time
 function changeDate(change) {
     if (change == "hourBack" && onScreenDate.isAfter(currentDateTime, 'hour')) {
@@ -190,7 +197,9 @@ function changeDate(change) {
         if (potentialNewDate.isSame(currentDateTime, 'day')) {
             if (potentialNewDate.isSameOrAfter(onScreenDate, 'hour')) {
                 onScreenDate.add(-1, 'days');
-            } else onScreenDate = currentDateTime.clone();
+            }
+            //send user back to current time, no further
+            else onScreenDate = currentDateTime.clone();
         } else {
             onScreenDate.add(-1, 'days');
         }
@@ -205,6 +214,7 @@ function changeDate(change) {
     updateView(bookings, onScreenDate);
 }
 
+//updates map based on bookings recorded in database
 function updateView(bookings, onScreenDate) {
     for (var i = 0; i < roomList.length; i++) {
         document.getElementById(roomList[i]).style.fill = AVAILABLE;
@@ -220,10 +230,6 @@ function updateView(bookings, onScreenDate) {
         var locationElement = document.getElementById(bookings[i][0]);
         if (onScreenDate.isBetween(startDateTime, endDateTime) || onScreenDate.isSame(startDateTime)) {
             locationElement.style.fill = UNAVAILABLE;
-            // }
-            // else {
-            //     locationElement.style.fill = AVAILABLE;
-            // }
         }
     }
 }
