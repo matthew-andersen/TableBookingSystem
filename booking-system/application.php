5ebccfe8bcc812638b40cc90706b465db3ef0f54
matthew-andersen <matthew.andersen@my.jcu.edu.au>
2016-12-14 12:11:51 +1000

    Fix style issues on left-column

--- booking-system/application.php
+++ booking-system/application.php
@@ -14,7 +14,6 @@ if (!isset($_SESSION['current_userid']) || empty($_SESSION['current_userid'])) {
     <script src="js/moment.js"></script>
     <script src="js/jquery-3.1.1.js"></script>
     <script src="js/script.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
