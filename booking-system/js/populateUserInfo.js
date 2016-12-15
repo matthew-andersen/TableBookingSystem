c0d81832fa05777161e4ef58763c6dd6f63dbf43
Draga <draga.doncilapop@my.jcu.edu.au>
2016-12-15 10:46:38 +1000

    removed extraneous file documented populateUserInfo.js

--- booking-system/js/populateUserInfo.js
+++ booking-system/js/populateUserInfo.js
@@ -1,8 +1,8 @@
-/**
+/**Script used to populate user account information (time allocations and confirmed bookings) on the dashboard
  * Created by Draga on 9/12/2016.
  */
-//use this file to get bookings from database (use user_id session variable to retrieve appropriate bookings)
-//populate "Current Bookings" with the information
+
+//get current datetime from system for use in determining which bookings to display (i.e. won't display bookings in the past)
 var todayDate = new Date();
 var currentYear = todayDate.getFullYear();
 var currentMonth = todayDate.getMonth();
@@ -14,13 +14,63 @@ var bookingContainer = document.getElementById('bookings');
 populateBookings();
 populateTimeAllocations();
 
+/**
+ * Function gets bookings from the database pertaining to the currently logged in user, and concatenates an
+ * appropriate display string then used to populate the "Your Bookings" portion of the dash
+ */
+function populateBookings() {
+    var arrayBookings = [];
 
+    var bookingRequest = new XMLHttpRequest();
+    bookingRequest.onload = function () {
+        //splits the booking string from the database into strings of the individual bookings
+        var strBookings = this.responseText.replace("\"", "").split("|");
+        //iterating from 1, first element blank
+        for (var i = 1; i < strBookings.length; i++) {
+            //split the individual booking strings into arrays of their own for convenience
+            var arrayBooking = strBookings[i].split(",");
+            arrayBookings.push(arrayBooking);
+        }
+    };
+    bookingRequest.open("GET", "booking-system/php/get_user_bookings.php", false);
+    bookingRequest.send();
+
+    for (var i = 0; i < arrayBookings.length; i++) {
+        var room_location = arrayBookings[i][0];
+        //Modify location string for display from room_x to Room x
+        room_location = room_location.replace("_", " ");
+        room_location = room_location.charAt(0).toUpperCase() + room_location.slice(1);
+
+        var startDateTime = arrayBookings[i][1];
+        var endDateTime = arrayBookings[i][2];
+        //convert string type dates into moments for easy formatting and comparison
+        momentStart = moment(startDateTime, "YYYY-MM-DD HH:mm:ss");
+        momentEnd = moment(endDateTime, "YYYY-MM-DD HH:mm:ss");
+
+
+        //if the current booking is today or after
+        if (momentStart.isSameOrAfter(currentDateTime, 'day')) {
+            var formatStart = momentStart.format('dddd, MMMM Do');
+            /*
+            * Concatenate an appropriate display string e.g.
+            * Sunday, December 18th:
+            * Room 3, 13:00 - 13:00
+            * */
+            var bookingString = formatStart + ": " + "<br>" + room_location + ", " + momentStart.format('HH:mm') + " - " + momentEnd.format('HH:mm');
+            bookingContainer.innerHTML += "<p>" + bookingString + "</p>";
+        }
+    }
+}
+
+/**
+ * Gets time allocations from user account in database pertaining to currently logged in user, populates the appropriate
+ * fields on the dash
+ */
 function populateTimeAllocations() {
     var accountInfo;
 
     var accountRequest = new XMLHttpRequest();
     accountRequest.onload = function () {
-        //what to you want to do with the response?
         accountInfo = this.responseText.split(',');
     };
     accountRequest.open("GET", "booking-system/php/get_user_account.php", false);
@@ -38,39 +88,5 @@ function populateTimeAllocations() {
 
 }
 
-
-function populateBookings() {
-    var arrayBookings = [];
-
-    var bookingRequest = new XMLHttpRequest();
-    bookingRequest.onload = function () {
-        //what to you want to do with the response?
-        var strBookings = this.responseText.replace("\"", "").split("|");
-        //iterating from 1, first element blank
-        for (var i = 1; i < strBookings.length; i++) {
-            var arrayBooking = strBookings[i].split(",");
-            arrayBookings.push(arrayBooking);
-        }
-    };
-    bookingRequest.open("GET", "booking-system/php/get_user_bookings.php", false);
-    bookingRequest.send();
-
-    for (var i = 0; i < arrayBookings.length; i++) {
-        var room_location = arrayBookings[i][0];
-        room_location = room_location.replace("_", " ");
-        room_location = room_location.charAt(0).toUpperCase() + room_location.slice(1);
-
-        var startDateTime = arrayBookings[i][1];
-        var endDateTime = arrayBookings[i][2];
-        momentStart = moment(startDateTime, "YYYY-MM-DD HH:mm:ss");
-        momentEnd = moment(endDateTime, "YYYY-MM-DD HH:mm:ss");
-
-        if (momentStart.isSameOrAfter(currentDateTime, 'day')){
-            var formatStart = momentStart.format('dddd, MMMM Do');
-            var bookingString = formatStart + ": " + "<br>" + room_location + ", " + momentStart.format('HH:mm') + " - " + momentEnd.format('HH:mm');
-            bookingContainer.innerHTML += "<p>" + bookingString + "</p>";
-        }
-    }
-}
 
 
