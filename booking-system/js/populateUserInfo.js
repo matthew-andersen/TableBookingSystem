0dbadda2ac698dd9426c018f10f8b30e4368f5f1
Draga <draga.doncilapop@my.jcu.edu.au>
2016-12-15 11:29:28 +1000

    removed unused images edited populateUserInfo.js to remove warnings for synchronous server requests

--- booking-system/js/populateUserInfo.js
+++ booking-system/js/populateUserInfo.js
@@ -22,44 +22,45 @@ function populateBookings() {
     var arrayBookings = [];
 
     var bookingRequest = new XMLHttpRequest();
+    bookingRequest.open("GET", "booking-system/php/get_user_bookings.php", true);
     bookingRequest.onload = function () {
-        //splits the booking string from the database into strings of the individual bookings
-        var strBookings = this.responseText.replace("\"", "").split("|");
-        //iterating from 1, first element blank
-        for (var i = 1; i < strBookings.length; i++) {
-            //split the individual booking strings into arrays of their own for convenience
-            var arrayBooking = strBookings[i].split(",");
-            arrayBookings.push(arrayBooking);
+        if (bookingRequest.readyState == 4 && bookingRequest.status == 200) {
+            //splits the booking string from the database into strings of the individual bookings
+            var strBookings = this.responseText.replace("\"", "").split("|");
+            //iterating from 1, first element blank
+            for (var i = 1; i < strBookings.length; i++) {
+                //split the individual booking strings into arrays of their own for convenience
+                var arrayBooking = strBookings[i].split(",");
+                arrayBookings.push(arrayBooking);
+            }
+            for (i = 0; i < arrayBookings.length; i++) {
+                var room_location = arrayBookings[i][0];
+                //Modify location string for display from room_x to Room x
+                room_location = room_location.replace("_", " ");
+                room_location = room_location.charAt(0).toUpperCase() + room_location.slice(1);
+
+                var startDateTime = arrayBookings[i][1];
+                var endDateTime = arrayBookings[i][2];
+                //convert string type dates into moments for easy formatting and comparison
+                var momentStart = moment(startDateTime, "YYYY-MM-DD HH:mm:ss");
+                var momentEnd = moment(endDateTime, "YYYY-MM-DD HH:mm:ss");
+
+
+                //if the current booking is today or after
+                if (momentStart.isSameOrAfter(currentDateTime, 'day')) {
+                    var formatStart = momentStart.format('dddd, MMMM Do');
+                    /*
+                     * Concatenate an appropriate display string e.g.
+                     * Sunday, December 18th:
+                     * Room 3, 13:00 - 13:00
+                     * */
+                    var bookingString = formatStart + ": " + "<br>" + room_location + ", " + momentStart.format('HH:mm') + " - " + momentEnd.format('HH:mm');
+                    bookingContainer.innerHTML += "<p>" + bookingString + "</p>";
+                }
+            }
         }
     };
-    bookingRequest.open("GET", "booking-system/php/get_user_bookings.php", false);
     bookingRequest.send();
-
-    for (var i = 0; i < arrayBookings.length; i++) {
-        var room_location = arrayBookings[i][0];
-        //Modify location string for display from room_x to Room x
-        room_location = room_location.replace("_", " ");
-        room_location = room_location.charAt(0).toUpperCase() + room_location.slice(1);
-
-        var startDateTime = arrayBookings[i][1];
-        var endDateTime = arrayBookings[i][2];
-        //convert string type dates into moments for easy formatting and comparison
-        var momentStart = moment(startDateTime, "YYYY-MM-DD HH:mm:ss");
-        var momentEnd = moment(endDateTime, "YYYY-MM-DD HH:mm:ss");
-
-
-        //if the current booking is today or after
-        if (momentStart.isSameOrAfter(currentDateTime, 'day')) {
-            var formatStart = momentStart.format('dddd, MMMM Do');
-            /*
-            * Concatenate an appropriate display string e.g.
-            * Sunday, December 18th:
-            * Room 3, 13:00 - 13:00
-            * */
-            var bookingString = formatStart + ": " + "<br>" + room_location + ", " + momentStart.format('HH:mm') + " - " + momentEnd.format('HH:mm');
-            bookingContainer.innerHTML += "<p>" + bookingString + "</p>";
-        }
-    }
 }
 
 /**
@@ -67,24 +68,26 @@ function populateBookings() {
  * fields on the dash
  */
 function populateTimeAllocations() {
-    var accountInfo=[];
+    var accountInfo = [];
 
     var accountRequest = new XMLHttpRequest();
     accountRequest.onload = function () {
-        accountInfo = this.responseText.split(',');
+        if (accountRequest.readyState == 4 && accountRequest.status == 200) {
+            accountInfo = this.responseText.split(',');
+
+            var userName = accountInfo[4];
+            var num_desk_hours = accountInfo[1];
+            var num_days = accountInfo[2];
+            var num_room_hours = accountInfo[3];
+
+            document.getElementById('user').innerHTML = userName;
+            document.getElementById('num_desk_hours').innerHTML = num_desk_hours;
+            document.getElementById('num_days').innerHTML = num_days;
+            document.getElementById('num_room_hours').innerHTML = num_room_hours;
+        }
     };
-    accountRequest.open("GET", "booking-system/php/get_user_account.php", false);
+    accountRequest.open("GET", "booking-system/php/get_user_account.php", true);
     accountRequest.send();
-
-    var userName = accountInfo[4];
-    var num_desk_hours = accountInfo[1];
-    var num_days = accountInfo[2];
-    var num_room_hours = accountInfo[3];
-
-    document.getElementById('user').innerHTML = userName;
-    document.getElementById('num_desk_hours').innerHTML = num_desk_hours;
-    document.getElementById('num_days').innerHTML = num_days;
-    document.getElementById('num_room_hours').innerHTML = num_room_hours;
 
 }
 
